68b484fb258ba2af1d17ef128c95eda62c93481e
HenryLyn <Lyn_2016@outlook.com>
2017-06-10 00:08:37 +0800

    Two-dementional array demo. A class for verifying the coldbach conjecture.

---
+++ Array/TwoDementionalArray/StudentScores.java
@@ -0,0 +1,49 @@
+/**
+*
+* @date 2017/06/09
+* @author Henry Lin
+* @description Using two-demensional array to save students scores.
+* @version 0.0.1.20170609
+*
+*/
+//import java.util.Random;
+
+public class StudentScores{
+    
+    public static void main(String[] args){
+        // Define a two-demensional array to save students scores.
+        // According to the follow order, core C++, core Java, Servlet, JSP and EJB.
+        // Randomly assign values.
+        int[][] studentsScores = new int[20][5];
+        int[] studentsTotalScores = new int[20];
+        for(int i = 0; i < 20; i++){
+            for(int j = 0; j < 5; j++){
+                studentsScores[i][j] = (int)(Math.random()*101);
+            }
+        }
+        
+        // Print students scores and compute the total scores.
+        System.out.println("StuNo \t Core C++ \t Core Java \t Servlet \t JSP \t EJB \t Total Score");
+        for(int i = 0; i < 20; i++){
+            System.out.print((i + 1) + " \t ");
+            int totalScore = 0;
+            for(int j = 0; j < 5; j++){
+                System.out.print(studentsScores[i][j] + " \t ");
+                totalScore += studentsScores[i][j];
+            }
+            studentsTotalScores[i] = totalScore;
+            System.out.print(studentsTotalScores[i]);
+            System.out.println();
+        }
+        
+        // Compute the students average score and print the detail.
+        System.out.println("Core C++ \t Core Java \t Servlet \t JSP \t EJB");
+        for(int i = 0; i < 5; i++){
+            int sum = 0;
+            for(int j = 0; j < 20; j++){
+                sum += studentsScores[j][i];
+            }
+            System.out.print((sum / 20) + " \t ");
+        }
+    }
+}
---
+++ VerifyingTheColdbachConjecture/VerifyingTheColdbachConjecture.java
@@ -0,0 +1,75 @@
+/**
+*
+* @date 2017/06/09
+* @author Henry Lin
+* @description Verifying the Goldbach conjecture.
+* @version 0.0.1.20170609
+*
+*/
+
+import java.util.Scanner;
+
+public class VerifyingTheColdbachConjecture{
+    
+    public static void main(String[] args){
+        
+        Scanner in = new Scanner(System.in);
+        boolean isError = false;
+        int number = -1;
+        while(true){
+            try{
+                // get a number by user.
+                System.out.println("Exit by -1.");
+                System.out.print("Please input a even number, bigger than 6: ");
+                number = in.nextInt();
+                
+            }catch(Exception e){
+                isError = true;
+                in = new Scanner(System.in);
+                System.out.println("Input error, please try again.");
+            }
+            // Check and verify the number.
+            if(isError){
+                isError = false;
+                continue;
+                
+            }else if(-1 == number){
+                break;
+                
+            }else if(number < 6){
+                System.out.println("Your number is less than 6, please try again.");
+                
+            }else if(checkIfTheNumberIsPrime(number)){
+                System.out.println("Your number is a prime number, please try again.");
+            
+            }else{
+                for(int left = 2; left < (number / 2); left++){
+                    if(checkIfTheNumberIsPrime(left)){
+                        int right = number - left;
+                        if(checkIfTheNumberIsPrime(right)){
+                            System.out.println(number + " = " + left + " + " + right);
+                        }
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+    
+    /**
+    *
+    * @description Check if the number is prime.
+    * @param number
+    * @return isPrime
+    *
+    */
+    public static boolean checkIfTheNumberIsPrime(int number){
+        double end = Math.sqrt(number);
+        for(int i = 2; i <= end; i++){
+            if(0 == (number % i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
